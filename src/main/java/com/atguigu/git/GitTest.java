4612ea4c63cb67fafd4583e7c2660a6c5b036ca5
pzd <2153219748@qq.com>
2023-07-22 11:38:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author pzd
+ * @date 2023-07-22-11:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
